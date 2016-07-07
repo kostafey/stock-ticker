6c7df0fe31de3c7c03723df2b8e97c0f7606b4b0
kostafey <kostafey@gmail.com>
2016-07-07 18:26:32 +0300

    Improve charts.

--- stock-ticker.el
+++ stock-ticker.el
@@ -124,9 +124,15 @@
   (let ((qs (assoc-default 'quote (assoc-default 'results (assoc-default 'query data)))))
     (mapcar
      (lambda (q)
-       (let ((close (assoc-default 'Adj_Close q))
-             (date (assoc-default 'Date q)))
-         close))
+       (let* ((close (string-to-number (assoc-default 'Adj_Close q)))
+              (high (string-to-number (assoc-default 'High q)))
+              (low (string-to-number (assoc-default 'Low q)))
+              (date (assoc-default 'Date q)))
+         (if (> close high)
+             (/ (+ high low) 2)
+           (if (< close low)
+               (/ (+ high low) 2)
+             close))))
      qs)))
 
 ;;;###autoload
@@ -174,6 +180,7 @@
                  (progn (setq stock-ticker--current-stocks
                               (stock-ticker--parse data))))))))
 
+;;;###autoload
 (defun stock-ticker--list ()
   "Request all required stock data list and display it to temporary buffer."
   (interactive)
@@ -189,6 +196,7 @@
                  (switch-to-buffer "*stock-ticker*")
                  (erase-buffer)
                  (insert (stock-ticker--parse-to-string data))
+                 (read-only-mode)
                  (message "Updated stock")))))
   (message "Requesting stock..."))
 
@@ -211,11 +219,12 @@
    :success (cl-function
              (lambda (&key data &allow-other-keys)
                (stock-ticker--draw-chart
-                (mapcar 'string-to-number
-                        (subseq (when data
-                                  (message "Updated stock")
-                                  (stock-ticker--parse-history data))
-                                0 100))
+                (when data
+                  (message "Updated stock")
+                  (let* ((parsed-data (stock-ticker--parse-history data))
+                         (ln (length parsed-data)))
+                    ;; Get last 200 days data
+                    (subseq (reverse parsed-data) (- ln 200) ln)))
                 "*stock-history*"))))
   (message "Requesting stock history..."))
 
@@ -266,72 +275,88 @@ the mode cycles through the requested symbols at a configurable interval."
                   (if (= prev 0) 1 0) (if (= new 0) 1 0)))))
 
 (defun stock-ticker--normalize-prices (prices)
-  (let ((mn (apply 'min prices))
-        (divisor (- (apply 'max prices) (apply 'min prices))))
+  (let* ((mn (apply 'min prices))
+         (ideal-delta 50)
+         (multiplier (/ ideal-delta (- (apply 'max prices) (apply 'min prices)))))
     (mapcar (lambda (p)
-              (round (- p mn)))
+              (round (* (- p mn) multiplier)))
             prices)))
+
+(defun stock-ticker-disply-buffer-below (buffer)
+  (let ((old-split split-width-threshold))
+    (setq split-width-threshold nil)
+    (get-buffer-create buffer)
+    (display-buffer buffer)
+    (setq split-width-threshold old-split)))
 
 (defun stock-ticker--draw-chart (prices buffer)
   "Draw chart for normalized PRICES list."
-  (switch-to-buffer buffer)
-  (erase-buffer)
-  (let* ((norm-prices (stock-ticker--normalize-prices prices))
-         (mi-origin (apply 'min prices))
-         (ma-origin (apply 'max prices))
-         (mi (apply 'min norm-prices))
-         (ma (apply 'max norm-prices))
-         (c 0)
-         (prev-pos nil)
-         (prev-dot nil))
-    (dotimes (number (1+ (ceiling (/ (- ma mi) 4.0))) nil)
-      (dotimes (number (length norm-prices) nil)
-        (insert
-         (propertize (drawille-draw-dot nil 0 0)
-                     'face
-                     `((:foreground ,(face-attribute 'default :background))))))
-      (insert "\n"))
-    (cl-labels ((draw-dot (price)
-                          (let* ((dot (mod (- price mi) 4))
-                                 (y (1- (ceiling
-                                         (abs
-                                          (/ (-
-                                              (* (+ (/ ma 4)
-                                                    (if (> (mod ma 4) 0) 1 0))
-                                                 4)
-                                              (- price mi))
-                                             4.0)))))
-                                 (x (ceiling (ffloor (/ c 2)))))
-                            (artist-move-to-xy x y)
-                            (delete-char 1)
-                            (if (equal (list x y) prev-pos)
-                                (insert (stock-ticker--join-dots prev-dot dot))
-                              (insert (drawille-draw-dot nil
-                                                         (if (oddp c) 1 0)
-                                                         dot)))
-                            (sit-for 0.01)
-                            (setq prev-dot dot)
-                            (setq prev-pos (list x y)))
-                          (setq c (1+ c))))
-      (mapcar
-       #'draw-dot
-       norm-prices))
-    (save-excursion
-      (artist-move-to-xy (+ (/ (length norm-prices) 2) 2) 0)
-      (insert (number-to-string ma-origin))
-      (artist-move-to-xy (+ (/ (length norm-prices) 2) 2)
-                         (ceiling (/ (- ma mi) 4.0)))
-      (insert (number-to-string mi-origin)))
-    (artist-forward-char 2)
-    (let ((l (car (last prices))))
-      (if (and (not (= l ma-origin))
-               (not (= l mi-origin)))
-          (progn
-            (insert (number-to-string l)))))
-    (end-of-buffer)))
+  (save-excursion
+    (stock-ticker-disply-buffer-below buffer)
+    (set-buffer buffer)
+    (setq truncate-lines t)
+    (erase-buffer)
+    (let* ((norm-prices (stock-ticker--normalize-prices prices))
+           (mi-origin (apply 'min prices))
+           (ma-origin (apply 'max prices))
+           (mi (apply 'min norm-prices))
+           (ma (apply 'max norm-prices))
+           (c 0)
+           (prev-pos nil)
+           (prev-dot nil))
+      (dotimes (number (1+ (ceiling (/ (- ma mi) 4.0))) nil)
+        (dotimes (number (length norm-prices) nil)
+          (insert
+           (propertize (drawille-draw-dot nil 0 0)
+                       'face
+                       `((:foreground ,(face-attribute 'default :background))))))
+        (insert "\n"))
+      (cl-labels ((draw-dot (price)
+                            (let* ((dot (mod (- price mi) 4))
+                                   (y (1- (ceiling
+                                           (abs
+                                            (/ (-
+                                                (* (+ (/ ma 4)
+                                                      (if (> (mod ma 4) 0) 1 0))
+                                                   4)
+                                                (- price mi))
+                                               4.0)))))
+                                   (x (ceiling (ffloor (/ c 2)))))
+                              (artist-move-to-xy x y)
+                              (delete-char 1)
+                              (if (equal (list x y) prev-pos)
+                                  (insert (stock-ticker--join-dots prev-dot dot))
+                                (insert (drawille-draw-dot nil
+                                                           (if (oddp c) 1 0)
+                                                           dot)))
+                              (sit-for 0.0001)
+                              (setq prev-dot dot)
+                              (setq prev-pos (list x y)))
+                            (setq c (1+ c))))
+        (mapcar
+         #'draw-dot
+         norm-prices))
+      (save-excursion
+        (artist-move-to-xy (+ (/ (length norm-prices) 2) 2) 0)
+        (insert (number-to-string ma-origin))
+        (artist-move-to-xy (+ (/ (length norm-prices) 2) 2)
+                           (ceiling (/ (- ma mi) 4.0)))
+        (insert (number-to-string mi-origin)))
+      (artist-forward-char 2)
+      (let ((l (car (last prices))))
+        (if (and (not (= l ma-origin))
+                 (not (= l mi-origin)))
+            (progn
+              (insert (number-to-string l)))))
+      (end-of-buffer))))
 
-;; (stock-ticker--history "AAPL")
-;; (stock-ticker--history "IBM")
+;;;###autoload
+(defun stock-ticker-get-history ()
+  (interactive)
+  (let ((symbol (nth (1- (line-number-at-pos)) stock-ticker-symbols)))
+    (when symbol
+      (message "symbol %s" symbol)
+      (stock-ticker--history symbol))))
 
 (provide 'stock-ticker)
 ;;; stock-ticker.el ends here
